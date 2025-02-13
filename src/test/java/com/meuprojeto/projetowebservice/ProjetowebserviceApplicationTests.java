925645fb6eac750e9e155e1cd3b686978416f8f7
Lucas Fonseca <dvlfnsc22@gmail.com>
2025-02-13 18:39:42 -0300

    Project created

---
+++ src/test/java/com/meuprojeto/projetowebservice/ProjetowebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuprojeto.projetowebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetowebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
