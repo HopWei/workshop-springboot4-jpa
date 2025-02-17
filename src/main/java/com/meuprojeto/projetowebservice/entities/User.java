2540f4bfa95555829c3fe26f6c57e475696ed29c
Lucas Fonseca <dvlfnsc22@gmail.com>
2025-02-17 17:17:24 -0300

    Order, Instant, ISO 8601 - initial

--- src/main/java/com/meuprojeto/projetowebservice/entities/User.java
+++ src/main/java/com/meuprojeto/projetowebservice/entities/User.java
@@ -1,18 +1,21 @@
 package com.meuprojeto.projetowebservice.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -22,8 +25,12 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
-	
-	public User() {}
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
+	public User() {
+	}
 
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
@@ -74,6 +81,10 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -89,5 +100,5 @@ public class User implements Serializable {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
-	}	
+	}
 }
