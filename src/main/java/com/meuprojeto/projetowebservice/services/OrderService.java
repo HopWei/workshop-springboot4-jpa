2e2cdfe39cade29ec8cce9b16f546cb1d80d05e1
Lucas Fonseca <dvlfnsc22@gmail.com>
2025-02-17 21:25:50 -0300

    Order, Instant, ISO 8601 - final

---
+++ src/main/java/com/meuprojeto/projetowebservice/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.meuprojeto.projetowebservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.meuprojeto.projetowebservice.entities.Order;
+import com.meuprojeto.projetowebservice.repositories.OrderRepository;
+
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
