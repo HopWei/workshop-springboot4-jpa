c6ee684db9c84677e9d33bde3ae65e455763b45b
Lucas Fonseca <dvlfnsc22@gmail.com>
2025-03-04 23:17:19 -0300

    Payment, one-to-one association

--- src/main/java/com/meuprojeto/projetowebservice/config/TestConfig.java
+++ src/main/java/com/meuprojeto/projetowebservice/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.meuprojeto.projetowebservice.entities.Category;
 import com.meuprojeto.projetowebservice.entities.Order;
 import com.meuprojeto.projetowebservice.entities.OrderItem;
+import com.meuprojeto.projetowebservice.entities.Payment;
 import com.meuprojeto.projetowebservice.entities.Product;
 import com.meuprojeto.projetowebservice.entities.User;
 import com.meuprojeto.projetowebservice.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 	
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		orderRepository.save(o1);
 	}
 
 }
